2eede2f17c436b12b47bf9e9ce74923fa6d904d4
sereins0618 <sereins0618@gmail.com>
2025-05-01 11:52:23 +0800

    opt:multisig accout accet msg cancel check

--- wallet-api/src/messaging/mqtt/topics/order/multisig_account/order_multisign_accept.rs
+++ wallet-api/src/messaging/mqtt/topics/order/multisig_account/order_multisign_accept.rs
@@ -111,12 +111,12 @@ impl OrderMultiSignAccept {
         let pool = Context::get_global_sqlite_pool()?;
         let mut repo = RepositoryFactory::repo(pool.clone());
         // 查询后端接口，判断是否账户已被取消
-        if Self::check_if_cancelled(&self.id).await? {
-            tracing::warn!(
-                event_name = %event_name,
-                "Multisig Account {} has been canceled",self.id);
-            return Ok(());
-        }
+        // if Self::check_if_cancelled(&self.id).await? {
+        //     tracing::warn!(
+        //         event_name = %event_name,
+        //         "Multisig Account {} has been canceled",self.id);
+        //     return Ok(());
+        // }
 
         let account = AccountRepoTrait::detail(&mut repo, &self.address).await?;
 
