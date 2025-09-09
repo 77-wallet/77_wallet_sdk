59ef7ffe507bf64ccebeaea1efae1f68f00e791f
sereins0618 <sereins0618@gmail.com>
2025-09-09 09:36:06 +0800

    sol:swap

--- wallet-api/src/domain/chain/adapter/sol_tx.rs
+++ wallet-api/src/domain/chain/adapter/sol_tx.rs
@@ -1,5 +1,5 @@
-use crate::domain::chain::swap::evm_swap::SwapParams;
-use wallet_chain_interact::sol::SolanaChain;
+use crate::{domain::chain::swap::evm_swap::SwapParams, request::transaction::DepositReq};
+use wallet_chain_interact::sol::{self, SolanaChain};
 
 pub(super) async fn estimate_swap(
     swap_params: &SwapParams,
@@ -12,5 +12,15 @@ pub(super) async fn swap(
     swap_params: &SwapParams,
     chain: &SolanaChain,
 ) -> Result<(), crate::ServiceError> {
+    Ok(())
+}
+
+pub(super) async fn deposit_fee(
+    chain: &SolanaChain,
+    req: &DepositReq,
+    amount: alloy::primitives::U256,
+) -> Result<(), crate::ServiceError> {
+    // let deposit = sol::operations::contract::Deposit::new(&req.from, &req.token, amount);
+
     Ok(())
 }
