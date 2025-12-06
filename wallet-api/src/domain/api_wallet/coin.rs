b68df7b72c88839a71c60ea71d53840eea7e5a7f
openg <openg@t.com>
2025-12-06 17:23:29 +0800

    fix: acc change and sync coin

--- wallet-api/src/domain/api_wallet/coin.rs
+++ wallet-api/src/domain/api_wallet/coin.rs
@@ -47,18 +47,24 @@ pub struct ApiCoinDomain {}
 
 impl ApiCoinDomain {
     pub async fn init_sync_api_coins() -> Result<(), crate::error::service::ServiceError> {
+        tracing::info!("init_sync_api_coins -------------------------------------------------------");
         let pool = crate::context::CONTEXT.get().unwrap().get_global_sqlite_pool()?;
         let coin_list = ApiCoinRepo::coin_list(&pool).await?;
         let backend_api = crate::context::CONTEXT.get().unwrap().get_global_backend_api();
+        let coins_find = backend_api.fetch_all_api_tokens(None,None).await?;
+
         for coin in &coin_list {
-            let price: f64 = coin.price.parse().unwrap_or_default();
-            if price != 0f64 {
-                continue;
-            }
+           let coin_find =  coins_find.iter().find(|o|o.token_address == coin.token_address
+            && o.chain_code == Some(coin.chain_code.clone()));
+            tracing::info!("init_sync_api_coins local coin: {:?},coin_find {:?}", coin,coin_find);
             if let Some(token) = &coin.token_address {
-                let coin_find = backend_api.token_price(&coin.chain_code, token).await.ok();
-                if let Some(coin) = coin_find {
-                    ApiCoinRepo::update_price_unit1(&coin.code, &coin.token_address, &price.to_string(), &pool).await?;
+                if let Some(coin_find) = &coin_find {
+                    let price: f64 = coin_find.price.unwrap_or_default();
+                    if price == 0f64 {
+                        continue;
+                    }
+                    tracing::info!("init_sync_api_coins  33: {:?},{:?},{:?}",coin.chain_code,token,price );
+                    ApiCoinRepo::update_price_unit1(&coin.chain_code, &token, &price.to_string(), &pool).await?;
                 }
             }
         }
