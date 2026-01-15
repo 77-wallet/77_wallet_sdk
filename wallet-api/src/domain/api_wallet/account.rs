24f96026f50c6afd83ce41e807fb6eda592b79bb
gin <gin@github.com>
2026-01-15 16:23:25 +0800

    chore: nothing

--- wallet-api/src/domain/api_wallet/account.rs
+++ wallet-api/src/domain/api_wallet/account.rs
@@ -246,7 +246,6 @@ impl ApiAccountDomain {
 
         let mut result: Vec<_> = vec![];
         for acc in account_assert {
-            tracing::info!("acc: {:?}", acc);
             let account_index_map =
                 wallet_utils::address::AccountIndexMap::from_account_id(acc.account_id)?;
 
@@ -274,8 +273,6 @@ impl ApiAccountDomain {
                 // break;
             }
 
-            tracing::info!("has_chain: {:?}", has_chain);
-            tracing::info!("chain_vec: {:?}", chain_vec);
             result.push(ApiAccountInfo {
                 chain: chain_vec,
                 account_id: acc.account_id,
