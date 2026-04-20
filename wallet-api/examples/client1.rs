68030b7753d17f7ba137723cf3c15eab9f17dbb9
gin <gin@github.com>
2026-04-20 16:12:32 +0800

    fix(api-wallet): use current api wallet list

--- wallet-api/examples/client1.rs
+++ wallet-api/examples/client1.rs
@@ -204,7 +204,7 @@ async fn test_balance(wallet_manager: Arc<wallet_api::manager::WalletManager>) {
 #[allow(unused)]
 async fn test_get_api_wallet_list() {
     let res =
-        wallet_api::domain::api_wallet::wallet::ApiWalletDomain::get_api_wallet_list_v2().await;
+        wallet_api::domain::api_wallet::wallet::ApiWalletDomain::get_api_wallet_list().await;
     tracing::info!("get_api_wallet_list: {res:#?}");
 }
 
