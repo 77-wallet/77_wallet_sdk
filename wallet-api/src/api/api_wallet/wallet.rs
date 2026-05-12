f30a716dd684ec38f785e982e15a18a796a83f39
gin <gin@github.com>
2026-05-12 18:59:22 +0800

    chore(api-wallet): enable api-mqtt by default and update test params

--- wallet-api/src/api/api_wallet/wallet.rs
+++ wallet-api/src/api/api_wallet/wallet.rs
@@ -268,7 +268,7 @@ mod test {
         // .to_string();
         // let salt = "7";
         // let salt = "q6666666";
-        let salt = "r0000007";
+        let salt = "r0000001";
         // let salt = "1234qwer";
         // let salt = "q6666668";
         let wallet_name = "api_wallet";
@@ -321,12 +321,12 @@ mod test {
         let invite_code = None;
         // let salt = "10";
         // let salt = "q2222222";
-        let salt = "w0000007";
+        let salt = "w0000001";
         // let salt = "q7777777";
         // let salt = "q7777780";
         // let salt = "1234qwer";
         // let binding_address = Some("0x17f6a199862FD0ffb2d5C79f3DBBE37597162A24");
-        let binding_address = Some("0x5489c657Be2504D657f1F56AB04abfE3C77ceC34");
+        let binding_address = Some("0x93A36df0a3716429C89EB01D56d269f66aFc26C6");
         // let binding_address = Some("0x7F90ff4374cDFEF97c7Fd546B5E038E06a528166");
         // let binding_address = Some("0x7fD535925bdeB6C1D77475B7f2F8E92475c45D95");
         let res = wallet_manager
